9674504a5ce3dac03040c89888c493d0f8e6fce1
Rakesh C V <rakesh.cv@hotmail.com>
2015-07-13 14:47:22 +1000

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ghPagesDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ghPagesDemoApp
+ */
+angular.module('ghPagesDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
